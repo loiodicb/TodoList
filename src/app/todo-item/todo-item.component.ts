c0453f3952e5b529abfaebf37c8f74cb4dd7383a
bryan <bryan.loiodice@gmail.com>
2020-11-29 19:52:16 +0100

    Second commit
    Add all the files of the project

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} Input, ViewChild, ElementRef  } from '@angular/core';
+import { TodoItemData } from '../dataTypes/TodoItemData';
+import { TodoService } from '../todo.service';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+
+export class TodoItemComponent implements OnInit {
+
+  @Input() private data: TodoItemData;
+  @ViewChild("newTextInput", { static: false }) private inputLabel: ElementRef;
+
+  private _editionMode = false;
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit() {
+  }
+
+  get editionMode(): boolean {
+    return this._editionMode;
+  }
+
+  set editionMode(e: boolean) {
+    this._editionMode = e;
+    requestAnimationFrame(() => this.inputLabel.nativeElement.focus());
+  }
+
+  get label(): string {
+    return this.data.label;
+  }
+
+  set label(lab: string) {
+    this.todoService.setItemsLabel(lab, this.data);
+  }
+
+  get isDone(): boolean {
+    return this.data.isDone;
+  }
+
+  set isDone(done: boolean) {
+    this.todoService.setItemsDone(done, this.data);
+  }
+
+  destroy() {
+    this.todoService.removeItems(this.data);
+  }
+
+}
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,102 @@
+import {Component, OnInit} from '@angular/core';
+import {TodoListData} from '../dataTypes/TodoListData';
+import {TodoItemData} from '../dataTypes/TodoItemData';
+import {TodoService} from '../todo.service';
+import { JsonPipe } from '@angular/common';
+import { VoiceRecognitionService } from '../voice-recognition.service';
+import { isEmpty } from 'rxjs-compat/operator/isEmpty';
+
+@Component({
+    selector: 'app-todo-list',
+    templateUrl: './todo-list.component.html',
+    styleUrls: ['./todo-list.component.css']
+ 
+})
+export class TodoListComponent implements OnInit {
+
+    private todoList: TodoListData;
+    public myAngularxQrCode: string = "";
+    public message : string = "";
+    
+    constructor(private todoService: TodoService, private voiceRecognition: VoiceRecognitionService){
+        todoService.getTodoListDataObservable().subscribe( tdl => this.todoList = tdl );
+        this.myAngularxQrCode = "";
+        this.voiceRecognition.init();
+        
+    }
+
+    ngOnInit() {
+    }
+ 
+    
+    get items(): TodoItemData[] {
+        return this.todoList.items;
+    }
+    appendItem(label: string){
+        if(label != ""){
+            this.todoService.appendItems({
+                label,
+                isDone:false,
+                isShow: true
+            });
+        }
+        
+    }
+    itemSupp(){
+        this.todoService.removeDoneItems();
+    }
+
+    itemActive(){
+       this.todoList.items.forEach(I => 
+        {if(I.isDone==true)
+            I.isShow=false;
+        else{I.isShow=true;}
+
+        });
+        
+    }
+
+    itemAll(){
+        this.todoList.items.forEach(I =>I.isShow=true);
+     }
+    
+    itemComplete(){
+        this.todoList.items.forEach(I => 
+        {if(I.isDone==true)
+            I.isShow=true;
+        else{
+            I.isShow=false;
+        }
+        });
+         
+    }
+    compteur(){
+        return this.todoList.items.length - this.todoList.items.filter(I =>I.isDone==true).length;
+    }
+    
+    erasedAll(){
+        this.todoList.items.forEach(I =>this.todoService.removeItems(I));
+    }
+
+    generateQrCode(){
+        this.myAngularxQrCode = JSON.stringify(this.todoList.items);
+        console.log(this.myAngularxQrCode);
+    }
+
+    start() {
+        this.voiceRecognition.start();
+    }
+    
+    stop(){
+        this.voiceRecognition.stop();
+        if(this.voiceRecognition.text != ""){
+            this.appendItem(this.voiceRecognition.text);
+            console.log("6 :",this.voiceRecognition.text);
+            // this.message = this.voiceRecognition.text;
+        }else{
+            console.log("variable empty");
+        }
+        this.voiceRecognition.text = "";
+        this.message = "";
+      }
+}
